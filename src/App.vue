58482c172426bdca4607aca2ac1015aeb7add44b
RiccardoSansone <r.sansone@gmail.com>
2023-09-29 17:23:15 +0200

    milestone 2

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ export default {
   data() {
     return {
       movie: '',
-      state
+      state,
     }
   },
   methods:{
@@ -22,13 +22,31 @@ export default {
   <input v-model="movie" type="text" placeholder="type film name" class="searchBar">
   <button @click="onClick" class="btn">Search</button>
   <ul>
+    FILM
     <li v-for="li in state.list">
       {{ li.title }}
       {{ li.original_title }}
       {{ li.original_language }}
       {{ li.vote_average }}
+      <img :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/'+li?.original_language?.toUpperCase()+'.svg'" alt="">
+    </li>
+
+  </ul>
+
+  <ul>
+    TV
+    <li v-for="li in state.listTv">
+      {{ li.name }}
+      {{ li.original_name }}
+      {{ li.original_language }}
+      {{ li.vote_average }}
+      <img :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/'+li?.origin_country[0]+'.svg'" alt="">
     </li>
   </ul>
+
+
+<!-- <i class="fa-solid fa-star"></i>    stella piena-->
+<!-- <i class="fa-regular fa-star"></i>  stella vuota -->
 
 </template>
 
--- src/state.js
+++ src/state.js
@@ -3,17 +3,22 @@ import axios from 'axios';
 
 export const state = reactive({
     list: [],
+    listTv: [],
 
     Search(movieToSearch){
-        let url = 'https://api.themoviedb.org/3/search/movie?query='+ movieToSearch +'&page=1'
-        const config = {
-            headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzdiMGVmY2JjNzAyOTlmYzk2OWM0M2IzNDE1NjAxNSIsInN1YiI6IjY1MTU0NjhmMWRiNjVkMDBlMzFjMjY4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8byVfYmSzCR-vJ2Gg_ejr8ZcDLe3KKYRoHXcZaRwsvs` }
-        };
+        let url = 'https://api.themoviedb.org/3/search/movie?api_key=977b0efcbc70299fc969c43b34156015&query='+ movieToSearch +'&page=1'
+        let url_TvSeries = 'https://api.themoviedb.org/3/search/tv?api_key=977b0efcbc70299fc969c43b34156015&query='+ movieToSearch +'&page=1'
         
-        axios.get(url, config)
+        axios.get(url)
         .then(res =>{
             this.list = res.data.results;
             console.log(this.list);
+
+            axios.get(url_TvSeries)
+            .then(respo =>{
+                this.listTv = respo.data.results;
+                console.log(this.listTv);
+            })
         })
     }
 })
