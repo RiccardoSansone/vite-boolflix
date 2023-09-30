c5ba6cf580decd7d55e9a0aa3a15666cca55edb0
RiccardoSansone <r.sansone@gmail.com>
2023-09-30 19:04:43 +0200

    add poster path

--- src/App.vue
+++ src/App.vue
@@ -46,39 +46,58 @@ export default {
         <div class="titolo">
           <h1>FILM</h1>
         </div>
+
         <div v-for="li in state.list" class="card">
-          <h5><i>Titolo:</i> {{ li.title }}</h5>
-          <h5><i>Titolo Originale:</i> {{ li.original_title }}</h5>
-          <h5><i>Lingua Originale:</i> {{ li.original_language }}</h5>
-           <h5><i>Voto:</i></h5>
-           <div class="star">
-            <div v-for="i in [1,2,3,4,5]">
-              <i  v-show="i <= Math.floor(li.vote_average/2)" class="fa-solid fa-star"></i>
-              <i  v-show="i > Math.floor(li.vote_average/2)" class="fa-regular fa-star"></i>
+
+          <div class="containerImg">
+            <img :src="'https://image.tmdb.org/t/p/w342' + li.poster_path" alt="">
+          </div>
+
+          <div class="containerInfo">
+            <h5><i>Titolo:</i> {{ li.title }}</h5>
+            <h5><i>Titolo Originale:</i> {{ li.original_title }}</h5>
+            <h5><i>Lingua Originale:</i> {{ li.original_language }}</h5>
+            <h5><i>Voto:</i></h5>
+            <div class="star">
+              <div v-for="i in [1,2,3,4,5]">
+                <i  v-show="i <= Math.floor(li.vote_average/2)" class="fa-solid fa-star"></i>
+                <i  v-show="i > Math.floor(li.vote_average/2)" class="fa-regular fa-star"></i>
+              </div>
             </div>
-           </div>
-          <img :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/'+li?.original_language?.toUpperCase()+'.svg'" alt="">
+            <img :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/'+li?.original_language?.toUpperCase()+'.svg'" alt="">
+          </div>
+
         </div>
         
       </div>
       <!-- /film -->
 
       <div class="tv">
+
         <div class="titolo">
-        <h1>TV</h1>
-      </div>
+          <h1>TV</h1>
+        </div>
+
         <div v-for="li in state.listTv" class="card">
-           <h5><i>Nome:</i> {{ li.name }}</h5>
-           <h5><i>Nome originale:</i> {{ li.original_name }}</h5>
-           <h5><i>Lingua originale:</i> {{ li.original_language }}</h5>
-           <h5><i>Voto:</i></h5>
-           <div class="star">
-            <div v-for="i in [1,2,3,4,5]">
-              <i  v-show="i <= Math.floor(li.vote_average/2)" class="fa-solid fa-star"></i>
-              <i  v-show="i > Math.floor(li.vote_average/2)" class="fa-regular fa-star"></i>
+
+          
+          <div class="containerImg">
+            <img :src="'https://image.tmdb.org/t/p/w342' + li.poster_path" alt="">
+          </div>
+
+          <div class="containerInfo">
+            <h5><i>Nome:</i> {{ li.name }}</h5>
+            <h5><i>Nome originale:</i> {{ li.original_name }}</h5>
+            <h5><i>Lingua originale:</i> {{ li.original_language }}</h5>
+            <h5><i>Voto:</i></h5>
+            <div class="star">
+              <div v-for="i in [1,2,3,4,5]">
+                <i  v-show="i <= Math.floor(li.vote_average/2)" class="fa-solid fa-star"></i>
+                <i  v-show="i > Math.floor(li.vote_average/2)" class="fa-regular fa-star"></i>
+              </div>
             </div>
-           </div>
-           <img :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/'+li?.origin_country[0]+'.svg'" alt="">
+            <img :src="'https://purecatamphetamine.github.io/country-flag-icons/3x2/'+li?.origin_country[0]+'.svg'" alt="">
+          </div>
         </div>
 
       </div>
@@ -88,10 +107,8 @@ export default {
     <!-- /container -->
 
   </main>
+  <!-- /main -->
 
-
-<!--     stella piena-->
-<!-- <i class="fa-regular fa-star"></i>  stella vuota -->
 
 </template>
 
--- src/state.js
+++ src/state.js
@@ -8,6 +8,7 @@ export const state = reactive({
     Search(movieToSearch){
         let url = 'https://api.themoviedb.org/3/search/movie?api_key=977b0efcbc70299fc969c43b34156015&query='+ movieToSearch +'&page=1'
         let url_TvSeries = 'https://api.themoviedb.org/3/search/tv?api_key=977b0efcbc70299fc969c43b34156015&query='+ movieToSearch +'&page=1'
+        let halfUrl = 'https://image.tmdb.org/t/p/w342'
         
         axios.get(url)
         .then(res =>{
